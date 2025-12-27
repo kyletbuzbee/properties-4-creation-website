a9937fce8a4325401711eb6d017c8cc39dd4fa6d
kyle buzbee <kyle.thomas.buzbee@gmail.com>
2025-12-27 09:35:49 -0600

    more layout fixes

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./about/**/*.html",
+    "./apply/**/*.html",
+    "./contact/**/*.html",
+    "./faq/**/*.html",
+    "./impact/**/*.html",
+    "./privacy/**/*.html",
+    "./properties/**/*.html",
+    "./resources/**/*.html",
+    "./terms/**/*.html",
+    "./transparency/**/*.html",
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
